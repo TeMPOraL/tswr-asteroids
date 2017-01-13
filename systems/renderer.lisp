6cbcd80dbb66ae65a58d99bfa6b3849e6d68334f
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2017-01-13 21:37:00 +0100

    Update because of changes in P2D engine structure.

--- systems/renderer.lisp
+++ systems/renderer.lisp
@@ -16,42 +16,42 @@
       ;;
       (gl:load-identity)
       (gl:with-pushed-matrix
-        (p2dg:translate2 position)
+        (p2dglu:translate2 position)
         (gl:with-pushed-matrix
           (if-let ((orientation (p2de:find-component entity 'orientation)))
-            (p2dg:rotatez* (orientation orientation)))
-          (p2dg:scale2-uniform scale)
-          (p2dg:color4 color)
+            (p2dglu:rotatez* (orientation orientation)))
+          (p2dglu:scale2-uniform scale)
+          (p2dglu:color4 color)
           (draw-pseudo-sprite sprite))
         (when *debug-render-kinematics*
           (when-let ((kinematics (p2de:find-component entity 'kinematics)))
             (gl:with-pushed-matrix
-              (p2dg:color4 +debug-color-velocity+)
+              (p2dglu:color4 +debug-color-velocity+)
               (draw-vector-marker-to-point (slot-value kinematics 'velocity))
-              (p2dg:color4 +debug-color-acceleration+)
+              (p2dglu:color4 +debug-color-acceleration+)
               (draw-vector-marker-to-point (slot-value kinematics 'acceleration)))))
         (when *debug-render-collision*
           (when-let ((bounding-sphere (p2de:find-component entity 'collision-sphere)))
             (gl:with-pushed-matrix
-              (p2dg:color4 (make-color-from-layer (slot-value bounding-sphere 'layer)))
-              (p2dg:scale2-uniform (slot-value bounding-sphere 'radius))
-              (p2dg:draw-circle-outline))))))))
+              (p2dglu:color4 (make-color-from-layer (slot-value bounding-sphere 'layer)))
+              (p2dglu:scale2-uniform (slot-value bounding-sphere 'radius))
+              (p2dglu:draw-circle-outline))))))))
 
 (defun draw-pseudo-sprite (sprite)
   "Pseudo because it's not really a `SPRITE', but rather immediate-mode geometry."
   (case sprite
-    (:asteroid (p2dg:draw-regular-polygon-outline 7))
+    (:asteroid (p2dglu:draw-regular-polygon-outline 7))
     (:ship (gl:scale 1 1.5 1)
-           (p2dg:draw-triangle))
+           (p2dglu:draw-triangle))
     (:ship-accelerating (gl:scale 1 1.5 1)
-                        (p2dg:draw-triangle)
+                        (p2dglu:draw-triangle)
                         (gl:scale 0.5 1.5 0.5)
-                        (p2dg:draw-triangle))
-    (:bullet (p2dg:draw-circle-outline :resolution 16))
-    (:powerup (p2dg:rotatez* (p2dm:deg->rad 45.0))
-              (p2dg:scale2-uniform 0.75)
-              (p2dg:draw-square-outline))
-    (t (p2dg:draw-square))))
+                        (p2dglu:draw-triangle))
+    (:bullet (p2dglu:draw-circle-outline :resolution 16))
+    (:powerup (p2dglu:rotatez* (p2dm:deg->rad 45.0))
+              (p2dglu:scale2-uniform 0.75)
+              (p2dglu:draw-square-outline))
+    (t (p2dglu:draw-square))))
 
 (defun draw-vector-marker-to-point (point)
   "Draws a vector marker from (0 0) to `POINT'."
